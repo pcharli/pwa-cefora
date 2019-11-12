013937d39d6bc50468c01987549d0733e6e123b5
Pierre Charlier <chrprr1@forem.be>
2019-11-12 11:52:30 +0100

    ajout cache dans service.js

--- service.js
+++ service.js
@@ -13,3 +13,28 @@ self.addEventListener("activate", event => {
  self.addEventListener('fetch', function () {
      //met en écoute le service
  })
+
+ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+ if (workbox) {
+     console.log('OK, workbox chargé');
+     workbox.precaching.precacheAndRoute([
+         {
+             "url" : "index.html"
+         },
+         {
+            "url" : "style.css"
+        },
+        {
+            "url" : "manifest.json"
+        },
+        {
+            "url" : "icon-96-96.png"
+        },
+        {
+            "url" : "favicon.ico"
+        },
+     ])
+ } else {
+     console.log('Oooh, workbox non chargé');
+ }
